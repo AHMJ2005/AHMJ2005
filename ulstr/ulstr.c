2baa2b0c33c2844a43c81073aac9233edf0ea923
kali linux <ahmad@kali.ahmad>
2025-09-30 12:00:13 -0400

    done

--- ulstr/ulstr.c
+++ ulstr/ulstr.c
@@ -7,28 +7,23 @@ int main(int argc , char **argv)
 	int i = 0;
 	char *a;
 	a = argv[1];
-	printf("%d" , argc);
-	if(argc > 1)
+	if(argc > 2)
 		write (1 , "\n" , 1);
-	//else 
-	//{
-		//write(1 , "aa", 2);
+	else 
+	{
 		while (a[i] != '\0')
 		{
 		if (a[i] >= 'a' && a[i] <= 'z' )
 		{
 			a[i] -= 32;
-			//write(1, "H" , 1);
 		}else if(a[i] >= 'A' && a[i] <= 'Z')
 		{
 			a[i] += 32;
-			//write(1 , &argv[1][i] , 1);
 		}
-			//write(1 , &argv[1][i] , 1);
 		i++;
 		}
 		a[i] = '\0';
-	//}
+	}
 	int j =0 ;
 	while (a[j] != '\0')
 	{
